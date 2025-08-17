909fc83931a6170507035d2dee0fd3df8ae5db32
Nathan Coleman <149843063+nathan-coleman@users.noreply.github.com>
2025-08-16 22:15:49 -0700

    Add basic logic to game creation UI

---
+++ game_scene/game_creation_elements/CreateGameController.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+namespace IndieGameDev.Game.UI;
+
+public partial class CreateGameController : Node
+{
+    private TabContainer gameOptionSelectionTabContainer;
+
+    public override void _Ready()
+    {
+        ConnectButtons();
+    }
+
+    private void ConnectButtons()
+    {
+        gameOptionSelectionTabContainer = GetNode<TabContainer>("%GameOptionSelectionTabContainer");
+
+        GetNode<Button>("%OpenGenrePaneButton").Pressed += OpenGenrePane;
+        GetNode<Button>("%OpenTopicPaneButton").Pressed += OpenTopicPane;
+        GetNode<Button>("%OpenPlatformPaneButton").Pressed += OpenPlatformPane;
+        GetNode<Button>("%OpenGraphicsPaneButton").Pressed += OpenGraphicsPane;
+        GetNode<Button>("%OpenTargetAudiencePaneButton").Pressed += OpenTargetAudiencePane;
+    }
+
+    private void OpenGenrePane() => gameOptionSelectionTabContainer.CurrentTab = 0;
+    private void OpenTopicPane() => gameOptionSelectionTabContainer.CurrentTab = 1;
+    private void OpenPlatformPane() => gameOptionSelectionTabContainer.CurrentTab = 2;
+    private void OpenGraphicsPane() => gameOptionSelectionTabContainer.CurrentTab = 3;
+    private void OpenTargetAudiencePane() => gameOptionSelectionTabContainer.CurrentTab = 4;
+}
